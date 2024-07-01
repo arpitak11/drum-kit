2cdebef2ae6291c8223563447039c667e6c3e42e
arpitak11 <157416586+arpitak11@users.noreply.github.com>
2024-07-01 23:24:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,17 @@ for(var i=0; i<numberOfLoop; i++){
 var soundName= 
 document.querySelectorAll("button")[i].addEventListener("click", function() {
     
-    var soundCommand= this.innerHTML;
-    switch(soundCommand){
+    var buttonInnerHTML= this.innerHTML;
+    makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
+});
+
+document.addEventListener("keypress", function(event) {
+    makeSound(event.key);
+    buttonAnimation(event.key);
+});
+function makeSound(key){
+    switch(key){
         case "w":
             var tom1= new Audio("sounds/tom-1.mp3");
             tom1.play();
@@ -42,7 +51,16 @@ document.querySelectorAll("button")[i].addEventListener("click", function() {
 
             default: soundCommand;
     }
-});
+}
+
+ function buttonAnimation(currentKey){
+    var activeButton= document.querySelector("."+ currentKey);
+    activeButton.classList.add("pressed");
+ 
+ setTimeout(function() {
+    activeButton.classList.remove("pressed")
+ }, 100);
 
 
+ }
 }
